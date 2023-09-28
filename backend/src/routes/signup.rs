f0d230d6dad3710ef06edeb7083e9a9ae35bcf6f
kelvinkirima014 <kelvinkirima2@gmail.com>
2023-09-28 21:57:42 +0300

    remove redudant response code in signup handler

--- backend/src/routes/signup.rs
+++ backend/src/routes/signup.rs
@@ -39,43 +39,4 @@ pub async fn signup(
     } else {
         Err(ApiError::InternalServerError)
     }
-
-
-
-
-
-    // if let Some(username) = &input.username {
-    //     if username.is_empty() {
-    //         return Ok(response::Response::builder()
-    //             .status(StatusCode::BAD_REQUEST)
-    //             .body(hyper::Body::from("Username can not be empty"))
-    //             .unwrap());
-    //     } else {
-    //         sqlx::query!(
-    //             r#"
-    //             INSERT INTO users (username, referral_code)
-    //             VALUES ($1, $2)
-    //             "#,
-    //             username,
-    //             new_referral_code
-    //         )
-    //         .execute(&ctx.db)
-    //         .await
-    //         .expect("Failed to insert into database");
-
-    //         return Ok(response::Response::builder()
-    //             .status(StatusCode::OK)
-    //             .body(Body::from("Signup succesful"))
-    //             .unwrap());
-    //     } 
-            
-       
-    // } else {
-    //     return Ok(response::Response::builder()
-    //     .status(StatusCode::BAD_REQUEST)
-    //     .body(Body::from("Missing username"))
-    //     .unwrap());
-
-    // }
-
 }
